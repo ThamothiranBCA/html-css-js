08522e62afdd6cb4c00653e64eabaf06e966cf04
unknown <thamuthamu478@gmail.com>
2023-10-03 11:25:39 +0530

    angularfirstpush

---
+++ Angular2.0/myApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularProjectComponent } from './angular-project/angular-project.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AngularProjectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
