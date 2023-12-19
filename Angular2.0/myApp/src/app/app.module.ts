1874e54b4289b7b8c9a7c4d95c2af4d5d61d58b0
thamothiran <thamuthamu478@gmail.com>
2023-12-19 11:38:55 +0530

    18dec

--- Angular2.0/myApp/src/app/app.module.ts
+++ Angular2.0/myApp/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularProjectComponent } from './angular-project/angular-project.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularProjectComponent
+    AngularProjectComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
